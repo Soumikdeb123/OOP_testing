9a723ceece1a7822dcdede15535b5a71ae44be6a
Soumikdeb123 <152465385+Soumikdeb123@users.noreply.github.com>
2024-04-29 11:26:37 +0930

    Update p1.py
    comments deleted

--- test1.py/p1.py
+++ test1.py/p1.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-# Constructing dictionary d using lists
+
 d = defaultdict(list)
 d['a'].append(1)
 d['a'].append(2)
@@ -8,7 +8,7 @@ d['a'].append(3)
 d['b'].append(4)
 d['b'].append(5)
 
-# Constructing dictionary e using sets
+
 e = defaultdict(set)
 e['a'].add(1)
 e['a'].add(2)
